ce4a4ffbce6bcdb64db6cf6be92739a174eec4c5
Lordcakes <Ahmetzyanovtimur3@gmail.com>
2020-11-04 23:15:45 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        <GameField/>
+    </div>
+</template>
+
+<script>
+    import GameField from "./components/GameField";
+    export default {
+        name: 'App',
+        components: {
+            GameField
+        }
+    }
+</script>
+
+<style>
+
+</style>
